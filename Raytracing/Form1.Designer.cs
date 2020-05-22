24d39c8b38397fd628ff4c5914f194834baea835
sizov1 <sizow.k.d@gmail.com>
2020-05-22 15:00:05 +0300

    ray tracing

---
+++ Raytracing/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace OpenGL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.openGlControl = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            // 
+            // openGlControl
+            // 
+            this.openGlControl.BackColor = System.Drawing.Color.Black;
+            this.openGlControl.Location = new System.Drawing.Point(9, 6);
+            this.openGlControl.Margin = new System.Windows.Forms.Padding(1);
+            this.openGlControl.Name = "openGlControl";
+            this.openGlControl.Size = new System.Drawing.Size(256, 256);
+            this.openGlControl.TabIndex = 0;
+            this.openGlControl.VSync = false;
+            this.openGlControl.Load += new System.EventHandler(this.openGlControl_Load);
+            this.openGlControl.Paint += new System.Windows.Forms.PaintEventHandler(this.openGlControl_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 272);
+            this.Controls.Add(this.openGlControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private OpenTK.GLControl openGlControl;
+    }
+}
+
